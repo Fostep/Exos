0c9a7c24b99ee42a5759ecbccc4d980347213dc5
Fostep <73890826+Fostep@users.noreply.github.com>
2020-11-17 14:28:51 +0100

    Update Tri.py

--- Tri.py
+++ Tri.py
@@ -19,28 +19,37 @@ def selectTri(liste):
   print(liste)
 
 #Tri par fusion 
-def fusion(gauche,droite):
-    resultat = []
-    index_gauche, index_droite = 0, 0
-    while index_gauche < len(gauche) and index_droite < len(droite):        
-        if gauche[index_gauche] <= droite[index_droite]:
-            resultat.append(gauche[index_gauche])
-            index_gauche += 1
-        else:
-            resultat.append(droite[index_droite])
-            index_droite += 1
-    if gauche:
-        resultat.extend(gauche[index_gauche:])
-    if droite:
-        resultat.extend(droite[index_droite:])
-    return resultat
-     
-def tri_fusion(m):
-    if len(m) <= 1:
-        return m
-    milieu = len(m) // 2
-    gauche = m[:milieu]
-    droite = m[milieu:]
-    gauche = tri_fusion(gauche)
-    droite = tri_fusion(droite)
-    return list(fusion(gauche, droite))
+def fusion(gauche, droite):
+  res = []
+  i = 0
+  j = 0
+
+  while i < len(gauche) and j < len(droite):
+    if(gauche[i] < droite[j]):
+      res.append(gauche[i])
+      i = i+1
+    else:
+      res.append(droite[j])
+      j = j+1
+
+  if(gauche):
+    res.extend(gauche[i:])
+  if(droite):
+    res.extend(droite[j:])
+  
+  return res
+
+def tri_fusion(tab, debut, fin):
+  if(len(tab) <= 1):
+    return tab
+  else:
+    milieu = (debut + fin)//2
+    #Decoupe gauche et droite
+    gauche = tab[:milieu]
+    droite = tab[milieu:]
+
+    #On continue de decouper ce qu'on à déjà découper
+    gauche = tri_fusion(gauche, 0, len(gauche))
+    droite = tri_fusion(droite, 0, len(droite))
+
+    return fusion(gauche, droite)
